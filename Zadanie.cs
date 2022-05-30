83355703f872d8642f39fd05dcbf95cc1bf239db
Kasia28-jk <kasian1999@interia.pl>
2022-05-30 18:01:35 +0200

    Added package

---
+++ Zadanie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Zadanie
+    {
+        public int ZadanieId { get; set; }
+        public string NazwaZadania { get; set; }
+        public int WartośćDoPoliczenia { get; set; }
+        public bool status { get; set; }
+    }
+}
